0ecafb50d5a2b12aca01ef5b200311f41842d366
Ben Brugman <BennyBrugman@hotmail.com>
2018-07-27 13:02:20 +0200

    final

--- src/test/java/ChapterSix/ValidateSupplierProductTest.java
+++ src/test/java/ChapterSix/ValidateSupplierProductTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.TreeMap;
 
 public class ValidateSupplierProductTest {
     WebElement elementToUse;
@@ -20,7 +21,7 @@ public class ValidateSupplierProductTest {
         ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
         String url = "https://techblog.polteq.com/testshop/index.php";
-
+        String productName = "MacBook Air";
 
         driver.get(url);
 
@@ -29,18 +30,21 @@ public class ValidateSupplierProductTest {
 
         List<WebElement> centreColumn = driver.findElements(By.className("right-block"));
 
+        boolean elementFound = false;
         for (WebElement element : centreColumn) {
 
-            if (element.getText().contains("MacBook Air")) {
-
-                assertThat(element.getText()).contains("MacBook Air").as("MacBook Air not in stock");
+            if (element.getText().contains(productName)) {
+                elementToUse = element;
+                assertThat(elementToUse.getText()).contains(productName).as("MacBook Air not in stock");
                 System.out.println("Element found");
-            } else if (element.getAttribute("title").contains("MacBook Air")) {
+                elementFound = true;
+            } else if (element.getAttribute("title").contains(productName)) {
                 System.out.println("Element found in Attribute");
+            } else {
+                System.out.println(element.getText() + " No such element found \n");
             }
-            else {
-                System.out.println(element.getText() + "No such element found");
-            }
+        }
+        assertThat(elementFound).isTrue().as("no object found to validate");
 
 
 //            WebElement textOnPage = element.findElement(By.cssSelector("#product_list > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.last-in-line.last-line.last-item-of-tablet-line.last-item-of-mobile-line.last-mobile-line"));
@@ -51,6 +55,6 @@ public class ValidateSupplierProductTest {
 
 //            assertThat(namePart.getText()).isEqualTo("MacBook Air").as("MacBook Air not in stock");
 //            assertThat(element.getText()).contains("MacBook Air").as("MacBook Air not in stock");
-        }
+
     }
 }
